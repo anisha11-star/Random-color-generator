f05d1c57f0adfaefc677f31722d16eca512c47a8
anisha-22 <anishak8765@gmail.com>
2026-07-01 22:39:57 +0530

    Added functionality using JavaScript

---
+++ app.js
@@ -0,0 +1,32 @@
+let todo =[];
+
+let req = prompt("Enter your request");
+while(true){
+    if(req == "quit"){
+        console.log("quitting the app");
+        break;
+    }
+   
+    if(req == "list"){
+        console.log("---------");
+        for(let i = 0 ; i < todo.length; i++){
+            console.log(i,todo[i]);
+        }
+        console.log("---------");
+    }
+    else if(req == "add"){
+        let task = prompt("Enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+        
+    }
+    else if(req == "delete"){
+        let idx = prompt("Enter index of the task you want to delete");
+        todo.splice(idx,1);
+        console.log("task deleted");
+    }
+    else{
+        console.log("Your request is invalid");
+    }
+    req = prompt("Enter your request");
+}
